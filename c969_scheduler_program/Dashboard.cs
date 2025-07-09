d54658b49e8fbda1a7be98631b20902eaa274879
Andrew Maloch <admaloch91@gmail.com>
2025-07-09 12:14:30 -0500

    added appt reminder

--- c969_scheduler_program/Dashboard.cs
+++ c969_scheduler_program/Dashboard.cs
@@ -1,6 +1,8 @@
 ﻿using c969_scheduler_program.Models;
 using c969_scheduler_program.Utils;
 using System;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace c969_scheduler_program
@@ -10,10 +12,16 @@ namespace c969_scheduler_program
         public Dashboard()
         {
             InitializeComponent();
-            OnDashboardLoad();
+            this.Load += MainAppointments_Load;
         }
 
-        private void OnDashboardLoad()
+        private void MainAppointments_Load(object sender, EventArgs e)
+        {
+            InitializeValues();
+            CreateApptReminderAlert();
+        }
+
+        private void InitializeValues()
         {
             usernameLbl.Text = $"Current user: {CurrentUser.UserName}";
             timeZoneLbl.Text = $"TimeZone: {Utilities.GetUserTimeZone()}";
@@ -21,6 +29,17 @@ namespace c969_scheduler_program
             usernameLbl.Left = (this.ClientSize.Width - usernameLbl.Width) / 2;
             regionLbl.Left = (this.ClientSize.Width - regionLbl.Width) / 2;
             timeZoneLbl.Left = (this.ClientSize.Width - timeZoneLbl.Width) / 2;
+        }
+
+        private async void CreateApptReminderAlert()
+        {
+            await Task.Delay(1500);
+            var todaysAppts = Appointment.GetAppointmentsForUserByDate(CurrentUser.UserId, DateTime.Today);
+            var upcomingAppt = todaysAppts.FirstOrDefault(appt => appt.Start > DateTime.Now && appt.Start < DateTime.Now.AddMinutes(25));
+            if (upcomingAppt != null)
+            {
+                MessageBox.Show($"Reminder!: You have an appointment at {upcomingAppt.Start.ToString("hh:mm")} with {upcomingAppt.CustomerName}", "Reminder", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
 
         private void customersBtn_Click(object sender, EventArgs e)
