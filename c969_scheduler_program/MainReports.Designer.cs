48d2fdbd4a94321ec031814d1b28c47e2cebbbe2
Andrew Maloch <admaloch91@gmail.com>
2025-07-11 09:34:32 -0500

    fixing currentUser

--- c969_scheduler_program/MainReports.Designer.cs
+++ c969_scheduler_program/MainReports.Designer.cs
@@ -32,6 +32,7 @@
             this.apptTypesBtn = new System.Windows.Forms.Button();
             this.userScheduleBtn = new System.Windows.Forms.Button();
             this.topCustomersBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -69,19 +70,31 @@
             // topCustomersBtn
             // 
             this.topCustomersBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.topCustomersBtn.Location = new System.Drawing.Point(189, 218);
+            this.topCustomersBtn.Location = new System.Drawing.Point(175, 218);
             this.topCustomersBtn.Name = "topCustomersBtn";
-            this.topCustomersBtn.Size = new System.Drawing.Size(173, 38);
+            this.topCustomersBtn.Size = new System.Drawing.Size(202, 38);
             this.topCustomersBtn.TabIndex = 3;
-            this.topCustomersBtn.Text = "Top customers";
+            this.topCustomersBtn.Text = "Customer engagement";
             this.topCustomersBtn.UseVisualStyleBackColor = true;
             this.topCustomersBtn.Click += new System.EventHandler(this.topCustomersBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitBtn.Location = new System.Drawing.Point(228, 262);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(90, 38);
+            this.exitBtn.TabIndex = 4;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
             // 
             // MainReports
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(542, 302);
+            this.ClientSize = new System.Drawing.Size(542, 344);
+            this.Controls.Add(this.exitBtn);
             this.Controls.Add(this.topCustomersBtn);
             this.Controls.Add(this.userScheduleBtn);
             this.Controls.Add(this.apptTypesBtn);
@@ -99,5 +112,6 @@
         private System.Windows.Forms.Button apptTypesBtn;
         private System.Windows.Forms.Button userScheduleBtn;
         private System.Windows.Forms.Button topCustomersBtn;
+        private System.Windows.Forms.Button exitBtn;
     }
 }
--- c969_scheduler_program/MainReports.cs
+++ c969_scheduler_program/MainReports.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace c969_scheduler_program
@@ -33,6 +26,11 @@ namespace c969_scheduler_program
         {
             CustomerEngagementForm frm = new CustomerEngagementForm();
             frm.Show();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
